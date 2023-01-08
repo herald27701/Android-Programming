d80450411c988baacebbfe11cbbe2ddcff49329f
herald27701 <quochuy181013@gmail.com>
2023-01-08 23:44:58 +0700

    first commit

---
+++ exercise1.kt
@@ -0,0 +1,30 @@
+import java.util.Scanner
+import java.util.Arrays
+
+val handshake = arrayOf("wink", "double blink", "close your eyes", "jump")
+
+fun secret(num:Int): String
+{
+    var slice = arrayOf<String>()
+    for (i in 0..handshake.size-1)
+    {
+        var result:Int = num and (1 shl i)
+        if (result != 0)
+        {
+            slice = slice.plus(handshake[i])
+        }
+        if ((num and (1 shl 4)) != 0)
+        {
+            slice.reverse()
+        }
+    }
+    return Arrays.toString(slice)
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    print("Enter a decimal number for secret handshake: ")
+    var num = sc.nextInt()
+    val result = secret(num)
+    println(result)
+}
